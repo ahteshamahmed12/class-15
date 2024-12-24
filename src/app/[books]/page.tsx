37d38ee7f4bd8f7f2a45df66e0c794eeb074722f
Your Name <you@example.com>
2024-12-24 17:41:47 +0500

    initial commit

---
+++ src/app/[books]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+async function page(props:any) {
+    let url=await fetch (`https://jsonplaceholder.typicode.com/todos/${props.params.books}`)
+    let responce=await url.json()
+    console.log(responce)
+    console.log(props)
+  return (
+   <div>
+    <h1 className='text-2xl' >
+        {responce.title} <span className='pl-3'>  ({responce.id})</span>
+    </h1>
+   </div>
+  )
+}
+
+export default page
